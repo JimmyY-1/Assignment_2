7abc94e8b0cdfa6bdc53dcecd44410db11fd5b97
JimmyY-1 <jimmyyim99@gmail.com>
2025-03-08 00:51:31 +0000

    Assignment Finished

---
+++ script.js
@@ -0,0 +1,39 @@
+/* CODE GOES HERE */
+import Legra from 'https://unpkg.com/legra?module';
+
+const slider = document.getElementById('range');
+const ctx = document.querySelector('canvas').getContext('2d');
+
+const loadImage = async (src) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.crossOrigin = "Anonymous";
+        img.addEventListener('load', () => {
+            resolve(img);
+        });
+        img.addEventListener('error', () => {
+            reject(new Error('Failed to load image'));
+        });
+        img.addEventListener('abort', () => {
+            reject(new Error('Image load aborted'));
+        });
+        img.src = src;
+    });
+};
+
+const drawMonalisa = async () => {
+    try {
+        const legra = new Legra(ctx, slider.value);
+        const img = await loadImage('starrynight.jpg');
+        ctx.clearRect(0, 0, 1200, 600);
+        legra.drawImage(img, [0, 0]);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+slider.addEventListener('input', () => {
+    drawMonalisa();
+});
+
+drawMonalisa();
